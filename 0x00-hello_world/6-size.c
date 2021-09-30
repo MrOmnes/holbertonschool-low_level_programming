701014302725c6e2a67192e19d2e7a3bf054e2c5
MrOmnes <3644@holbertonschool.com>
2021-09-30 10:26:06 +0000

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,14 +5,10 @@
 */
 int main(void)
 {
-	int intType;
-	float floatType;
-	double doubleType;
-	char charType;
-
-	printf("%zu", sizeof(intType));
-	printf("%zu", sizeof(floatType));
-	printf("%zu", sizeof(doubleType));
-	printf("%zu", sizeof(charType));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d bytes(s)\n", sizeof(float));
 	return (0);
 }
