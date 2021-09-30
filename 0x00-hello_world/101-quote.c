630ec385fb8e5f2a93ec3654cdda9deedaed1ce4
MrOmnes <3644@holbertonschool.com>
2021-09-30 15:03:04 +0000

    .

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+* main - Print the error text
+* Return: 1
+*/
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
