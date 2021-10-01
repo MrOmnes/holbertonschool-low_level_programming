1a175e440bc94a4b9d0bd849c4e1f28e20af5d2d
MrOmnes <3644@holbertonschool.com>
2021-10-01 20:30:21 +0000

    .

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Print 0 to 999 but not duplicate
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 0, b, d;
+
+	while (c < 9)
+	{
+		b = c + 1;
+		while (b <= 9)
+		{
+			c = d + 1;
+			while (d <= 9)
+			{
+				putchar(c + '0');
+				putchar(b + '0');
+				putchar(d + '0');
+				if (c < 7 || b < 8 || d < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				d;
+			}
+			b;
+		}
+		c;
+	}
+
+	putchar('\n');
+	return (0);
+}
