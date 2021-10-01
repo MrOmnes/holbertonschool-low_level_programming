f86397a49f5ba198330a18ef1f993b2ee0f2d2b6
MrOmnes <3644@holbertonschool.com>
2021-10-01 09:21:58 +0000

    .

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - Print Alphabet
+* Return: 0
+*/
+int main(void)
+{
+	char c = '0';
+	char C = 'a'; 
+
+	do
+	{
+		putchar(c);
+		++c;
+	}
+	while (c <= '9');
+	do
+	{
+		putchar(C);
+		++C;
+	}
+	while (C <= 'f');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - Print Numbers
+* Return: 0
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c => '9'; ++c)
+		putchar(c);
+	if (0 == '9')
+		{
+			putchar('\n');
+		}
+	else 
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	return (0);
+}
