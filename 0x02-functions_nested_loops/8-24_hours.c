6c6835265dfe07d1d7d12bb13114e8b5d558ff0b
MrOmnes <3644@holbertonschool.com>
2021-10-05 17:08:08 +0000

    24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Clock
+ * @h: Hours
+ * @m: Minutes
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void jack_bauer(void)
+{
+	int	h = 0, m;
+
+	while (h < 24)
+	{
+		m = 0;
+		while (m < 60)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		h++;
+	}
+}
