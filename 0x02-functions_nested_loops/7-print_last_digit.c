c040e1e33396505a8637152450e212f156013d40
MrOmnes <3644@holbertonschool.com>
2021-10-05 16:48:20 +0000

    .

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - ...
+ * @r: number who want to resolve
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_last_digit(int r)
+{
+	return (r % 10);
+}
