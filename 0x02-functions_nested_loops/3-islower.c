61999a9e9b70d61948ee7d2e21de6555310349be
MrOmnes <3644@holbertonschool.com>
2021-10-05 13:25:41 +0000

    .

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - writes the character c to stdout
+ * @c: Value to check
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _islower(int c)
+{
+
+	if (c >= 97 && c <= 172)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
