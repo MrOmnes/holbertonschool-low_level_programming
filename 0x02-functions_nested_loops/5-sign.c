a191e6d02ea3d524d83528957ef137204e9a3b1d
MrOmnes <3644@holbertonschool.com>
2021-10-05 13:45:06 +0000

    .

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - writes the character c to stdout
+ * @c: Value to check
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	return (0);
+	
+}
