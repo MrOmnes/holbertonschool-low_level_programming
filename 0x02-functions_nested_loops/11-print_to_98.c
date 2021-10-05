17c2fc856fef15ff5723278e5ced03220d95759e
MrOmnes <3644@holbertonschool.com>
2021-10-05 19:03:32 +0000

    .

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Affiche les chiffres jusqu'à 98
+ * @n: number who want to resolve
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_to_98(int n)
+{
+	int d = n;
+
+	while (d <= 98)
+	{
+		printf("%d, ", d);
+		d++;
+	}
+}
