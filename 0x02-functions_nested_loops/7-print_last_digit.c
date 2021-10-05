e698ae6f9daf481111d51fc7b6f6f5592c68f004
MrOmnes <3644@holbertonschool.com>
2021-10-05 16:53:05 +0000

    .

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,3 +10,15 @@ int print_last_digit(int r)
 {
 	return (_abs(r % 10));
 }
+
+/**
+ * _abs - returns absolute value of given int
+ * @n: number for which we want to get the absolute value
+ *
+ * Return: absolute value of number n
+ */
+
+int	_abs(int n)
+{
+	return ((n < 0) ? -n : n);
+}
