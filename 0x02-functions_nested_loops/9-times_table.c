58c7116975e2bd2a8eeeac82c6d366d66792b1db
MrOmnes <3644@holbertonschool.com>
2021-10-05 21:37:54 +0000

    .

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,23 +1,29 @@
 #include "main.h"
 
 /**
- * times_table - Print * tables
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-void print_number(int number)
+ * print_number - Print table with putchar of ten and unit
+ * @ten: Affiche le chiffre des dizaines
+ * @unit: Affiche le chiffre des unités
+ * @column: Valeur de la colone
+ * @number: Résultat de la fonction times_table
+*/
+void print_number(int number, int column)
 {
 	int ten = number / 10, unit = number % 10;
 
-	if (ten == 0)
+	if (ten == 0 && column != 0)
 		_putchar(' ');
 	else
 		_putchar(ten + 48);
 	_putchar(unit + 48);
 }
 
-
+/**
+ * times_table - Print * tables
+ * @row: Valeur de la ligne
+ * @column: Valeur de la colone
+ * @result: Valeur de la ligne * Valeur de la colone
+ */
 void times_table(void)
 {
 	int row, column, result;
@@ -27,7 +33,7 @@ void times_table(void)
 		for (column = 0; column < 10; column++)
 		{
 			result = row * column;
-			print_number(result);
+			print_number(result, column);
 			if (column != 9)
 			{
 				_putchar(',');
