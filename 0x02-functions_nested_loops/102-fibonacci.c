a8d8dd8f6e234774f30719e292b5d4a5291b7e19
MrOmnes <3644@holbertonschool.com>
2021-10-06 12:07:40 +0000

    .

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - Affiche les 50 premiers chiffres de la suite fibonacci
+* Return: 0
+*/
+int main(void)
+{
+	long int j = 1;
+	long int k = 2;
+	long int i;
+	long int next;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		printf("%ld, ", j);
+	next = j + k;
+	j = k;
+	k = next;
+	}
+}
