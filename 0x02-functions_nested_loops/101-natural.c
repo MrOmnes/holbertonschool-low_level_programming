7d07c3c60cecb5ff2e85475cbc99d685cbd1d9cb
MrOmnes <3644@holbertonschool.com>
2021-10-06 11:32:03 +0000

    .

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int t = 0;
+	int c = 0;
+	int multiple = 0;
+
+	for ( i = 0; i < 1024; ++i)
+	{
+		if (i % 3 == 0)
+		{
+			t = t + i;
+		}
+		if (c % 5 == 0)
+		{
+			c = c + i;
+		}
+	}
+	multiple = t + c;
+	printf("%d\n", multiple);
+}
