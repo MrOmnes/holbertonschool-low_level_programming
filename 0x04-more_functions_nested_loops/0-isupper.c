8b13b60518f96f3dd7683cc3ffe6eaa8c7c7702e
MrOmnes <3644@holbertonschool.com>
2021-10-07 07:44:31 +0000

    .

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*_isupper - Check if a number is upper or lower
+*@c: Number to check
+*Return: 1 if is upper, 0 otherwise
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	} 
+}
