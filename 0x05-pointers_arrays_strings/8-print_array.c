bd01db0fff326aa6511e888ae23f756a40bd1659
MrOmnes <3644@holbertonschool.com>
2021-10-11 11:55:10 +0000

    .

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+* print_array - Affiche l'ensemble des valeurs d'un array
+* @a: Valeurs a afficher
+* @n: Nombre de valeurs à afficher
+*/
+void print_array(int *a, int n)
+{
+	int c;
+
+	for (c = 0; c <= n; c++)
+	{
+		printf("%d, ", a[c]);
+	}
+	printf("\n");
+}
