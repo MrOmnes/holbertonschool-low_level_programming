e89133b9efa5943572d0804e4ce67c2cde334259
MrOmnes <3644@holbertonschool.com>
2021-10-11 11:28:29 +0000

    .

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+*puts_half - Print the second half of a string
+*@str: String to print
+*/
+void puts_half(char *str)
+{
+	int c;
+
+	for (c  = (_strlen(str) / 2); c <= _strlen(str); c++)
+	{
+		_putchar(str[c]);
+	}
+
+	_putchar('\n');
+}
+
+/**
+* _strlen - Print length of a string
+* @str: String to print lenght
+* Return: 0 if *s is not \0 and 1 when is it
+*/
+int _strlen(char *str)
+{
+	if (*str == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen(++str));
+	}
+}
