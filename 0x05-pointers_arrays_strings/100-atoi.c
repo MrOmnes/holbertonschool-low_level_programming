57254ccdad199e7d46e2f30c43c0833873afe504
MrOmnes <3644@holbertonschool.com>
2021-10-12 12:09:14 +0000

    .

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _atoi - Affiche le nombre en vérifiant s'il est positif ou négatif
+*@s: String a analyser
+*Return: nombre
+*/
+int _atoi(char *s)
+{
+	long int n = 0;
+	long int nombre = 0;
+	long int moins = 0;
+
+	while (!(s[n] >= 48 && s[n] <= 57))
+	{
+		if (s[n] == 45)
+		{
+			moins++;
+		}
+
+		n++;
+	}
+
+	while ((s[n] >= 48 && s[n] <= 57))
+	{
+		nombre = (nombre * 10) + (s[n] - 48);
+		n++;
+	}
+
+	if (moins % 2 != 0)
+	{
+		nombre *= -1;
+	}
+
+	return (nombre);
+}
