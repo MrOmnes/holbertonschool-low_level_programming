e8a80c67b1164fe87a7954a922e2aabed45b3e82
MrOmnes <3644@holbertonschool.com>
2021-10-20 08:43:25 +0000

    Fix recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,13 @@
 */
 void _puts_recursion(char *s)
 {
-	int size = 0;
-
-	while (s[size] != '\0')
+	if (*s == '\0')
 	{
-		_putchar(s[size]);
-		size++;
+		_putchar('\n');
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
---
+++ 0x08-recursion/_strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - Print length of a string
+* @s: String to print lenght
+* Return: 0 if *s is not \0 and 1 when is it
+*/
+int _strlen(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen(++s));
+	}
+}
