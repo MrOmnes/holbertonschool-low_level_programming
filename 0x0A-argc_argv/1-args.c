223325c3c16f203ce7e9d2a2612401079661d745
MrOmnes <3644@holbertonschool.com>
2021-10-22 12:25:06 +0000

    add 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main - Print argv
+* @argc: Array
+* @argv: Argument
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int count;
+
+  	if (argc > 0)
+    {
+      for (count = 0; count < argc; count++)
+	  {
+
+	  }
+
+	}
+	else
+	{
+		printf("%s", argv[0]);
+	}
+
+	printf("%d \n", count);
+	return (0);
+}
