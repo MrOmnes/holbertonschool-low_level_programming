c17b86a8c8c80705759597380ebeb0574ae165ff
MrOmnes <3644@holbertonschool.com>
2021-10-22 13:25:21 +0000

    Add 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Print argv
+* @argc: Array
+* @argv: Argument
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	if (argc >= 2)
+	{
+		int i = atoi(argv[1]);
+		int j = atoi(argv[2]);
+
+		printf("%d\n", i * j);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
