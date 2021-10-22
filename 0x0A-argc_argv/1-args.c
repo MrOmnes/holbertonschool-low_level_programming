ef70d27f97290fb4879cc50bc3a77f525492e61a
MrOmnes <3644@holbertonschool.com>
2021-10-22 12:45:43 +0000

    Fix 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,22 +6,9 @@
 * @argv: Argument
 * Return: 0
 */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__ ((unused)))
 {
-	int count;
+	printf("%d", argc - 1);
 
-	if (argc > 0)
-	{
-		for (count = 0; count < argc; count++)
-		{
-
-		}
-	}
-	else
-	{
-		printf("%s", argv[0]);
-	}
-
-	printf("%d \n", count - 1);
 	return (0);
 }
