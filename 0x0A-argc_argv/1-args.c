4052634fc95f22920765b1de7d00abc6c2807538
MrOmnes <3644@holbertonschool.com>
2021-10-22 12:26:12 +0000

    Fix betty

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,13 +10,12 @@ int main(int argc, char *argv[])
 {
 	int count;
 
-  	if (argc > 0)
-    {
-      for (count = 0; count < argc; count++)
-	  {
+	if (argc > 0)
+	{
+		for (count = 0; count < argc; count++)
+		{
 
-	  }
-
+		}
 	}
 	else
 	{
