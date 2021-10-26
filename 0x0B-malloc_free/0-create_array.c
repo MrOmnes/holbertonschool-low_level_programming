ca1124b13f322fcf38e6f28222305b85668d6052
MrOmnes <3644@holbertonschool.com>
2021-10-26 10:25:41 +0000

    Fix Malloc Return ?

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 	}
 	if (size != 0)
 	{
-		s = malloc((size) * sizeof(char));
+		s = (char *)malloc((size) * sizeof(char));
 		i = 0;
 		while (i < size)
 		{
