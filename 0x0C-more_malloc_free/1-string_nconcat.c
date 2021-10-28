6b7d2b0db3906929f499db117f8273727690ce34
MrOmnes <3644@holbertonschool.com>
2021-10-28 11:28:34 +0000

    Try to fix 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -64,7 +64,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (_strdup(s2));
 	if (s2 == NULL && s1 != NULL)
 		return (_strdup(s1));
-	str = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
+	str = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 6));
 	if (!str)
 		return (NULL);
 	if (s1 != NULL && s2 != NULL)
