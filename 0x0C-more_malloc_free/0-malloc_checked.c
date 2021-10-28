3ac3c4e6efed99a7de3c2ea48bac4295bb91e6e7
MrOmnes <3644@holbertonschool.com>
2021-10-28 10:52:52 +0000

    Fix Betty

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,11 +4,12 @@
 /**
  * malloc_checked - Check if a malloc function work
  * @b: Size of the malloc
- * Return 98 if error, else pointer adress
+ * Return: 98 if error, else pointer adress
 */
 void *malloc_checked(unsigned int b)
 {
 	int *u;
+
 	u = malloc(b);
 	if (u == NULL)
 		exit(98);
