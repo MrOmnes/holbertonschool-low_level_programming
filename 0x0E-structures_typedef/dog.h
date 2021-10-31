b0129cb7759da8ed31dbfc3af94631ff5bb7e27a
MrOmnes <3644@holbertonschool.com>
2021-10-31 15:03:04 +0000

    Add 0 and readme

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef dog
+#define dog
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - Structure Dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+*/
+	struct dog
+	{
+		char *name;
+		float age;
+		char *owner;
+
+	};
+
+#endif
