b3765f7818760f76c661903bd5631f92f819e958
MrOmnes <3644@holbertonschool.com>
2021-11-12 16:59:01 +0000

    Fix 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,22 +7,21 @@
 **/
 size_t print_list(const list_t *h)
 {
-	char *str;
 	unsigned int node = 0;
 	int i;
 
-	if (str == NULL)
+	if (h->str == NULL)
 	{
 		printf("[%d] (nil)\n", 0);
+		node ++;
 		h = h->next;
-		node += 0;
 	}
 
 	for (i = 0; h != NULL; i++)
 	{
 		printf("[%d] %s \n", h->len, h->str);
-		h = h->next;
 		node++;
+		h = h->next;
 	}
 
 	return (node);
