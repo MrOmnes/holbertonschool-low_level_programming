c3f4f393db274d410c5159f782105e97a8041db6
MrOmnes <3644@holbertonschool.com>
2021-11-12 19:41:41 +0000

    fix 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,9 +18,26 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = _strlen(str);
+	new->len = _strlen(new->str);
 	new->next = *head;
 	*head = new;
 
 	return (*head);
+}
+
+/**
+* _strlen - Print length of a string
+* @s: String to print lenght
+* Return: 0 if *s is not \0 and 1 when is it
+*/
+int _strlen(const char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen(++s));
+	}
 }
