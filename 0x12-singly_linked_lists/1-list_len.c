5c9da06a4532a9943311f627bfc96c55e20b8811
MrOmnes <3644@holbertonschool.com>
2021-11-12 18:40:29 +0000

    Add 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * print_list - Return number of node
+ * @h: List to print
+ * Return: number of node
+**/
+size_t list_len(const list_t *h)
+{
+	char *str;
+	const list_t *tmp = h;
+	size_t i;
+
+	if (tmp == NULL)
+		return (0);
+
+	for (i = 0; tmp != NULL; i++)
+	{
+
+		str = tmp->str;
+
+		if (str == NULL)
+		{
+			str = "(nil)";
+		}
+		tmp = tmp->next;
+	}
+
+	return (i);
+}
