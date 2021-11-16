198b6bbfc6a1b5bbd1caad0d820232c40af40809
MrOmnes <3644@holbertonschool.com>
2021-11-16 10:11:32 +0000

    Fix betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+
+/**
+ * print_listint - Print the number of nodes
+ * @h: Head of the node
+ * Return: Size of the node
+ */
 size_t print_listint(const listint_t *h)
 {
 	int number_of_nodes = 0;
