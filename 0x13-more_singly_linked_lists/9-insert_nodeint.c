123b152b91be44ef8b5d92d6b85185e16aa1d3ee
MrOmnes <3644@holbertonschool.com>
2021-11-17 13:14:37 +0000

    Try a fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -42,7 +42,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = temp->next;
 	}
 
-	if (temp == NULL)
+	if (i < idx)
 		return (NULL);
 
 	previous->next = new;
