ee0481f57844fb682e43603d6d73cb3b45e6fe30
MrOmnes <3644@holbertonschool.com>
2021-11-19 09:55:04 +0000

    Add 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*print_binary - Transform a long unsigned int too binary
+*
+*@n: int to transform
+*Return: VOID
+*/
+void print_binary(unsigned long int n)
+{
+	int i = 0;
+	char s[90];
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+
+	for (i = 0; n > 0; i++)
+	{
+		s[i] = n % 2;
+		n = n / 2;
+	}
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i] + 48);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,8 +2,11 @@
 #define MAIN_H
 #include <stdio.h>
 #include <math.h>
+#include <unistd.h>
 
 unsigned int binary_to_uint(const char *b);
 int _strlen(const char *s);
+void print_binary(unsigned long int n);
+int _putchar(char c);
 
 #endif
