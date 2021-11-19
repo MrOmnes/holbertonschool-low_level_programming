5857aa0a58a167bb29a3c180b6c62781d33e38d0
MrOmnes <3644@holbertonschool.com>
2021-11-19 12:14:42 +0000

    Add 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* get_bit - Get the bit object
+*
+*@n: Int where get the bit
+*@index: Where is the number where we want to get the bit
+*Return: The bit
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > n)
+		return (-1);
+	n = n >> index;
+	n = n & 1;
+
+	return (n);
+}
