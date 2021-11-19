6802d33908944db23de516058497107ab2ae5b68
MrOmnes <3644@holbertonschool.com>
2021-11-19 17:18:04 +0000

    Add 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* flip _bits - Count how many flip to have the same int
+*
+*@n: Int 1
+*@m: Int 2
+*Return: Number of diffrent bit
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int check = n ^ m;
+	unsigned int i = 0;
+
+	while (check > 0)
+	{
+		i += (check & 1) ? 1 : 0;
+		check = check >> 1;
+	}
+
+	return (i);
+}
