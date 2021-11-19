ec6b5421f28b940cd26feb45fd34a6a46faebf3e
MrOmnes <3644@holbertonschool.com>
2021-11-19 11:07:31 +0000

    Fix 1 thanks Mickael

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,19 +8,7 @@
 */
 void print_binary(unsigned long int n)
 {
-	int i = 0;
-	char s[90];
-
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-
-	for (i = 0; n > 0; i++)
-	{
-		s[i] = n & 1;
-		n = n >> 1;
-	}
-	for (i = i - 1; i >= 0; i--)
-		_putchar(s[i] + 48);
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + 48);
 }
