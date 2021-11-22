78f6f9e250ee53e008b8ec50d940dfa804c3a576
MrOmnes <3644@holbertonschool.com>
2021-11-22 16:22:21 +0000

    Add a condition

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -42,6 +42,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		fo = open(filename, O_WRONLY | O_APPEND);
 
+		if (fo == -1)
+			return (-1);
+
 		if (text_content == NULL)
 			return (1);
 
