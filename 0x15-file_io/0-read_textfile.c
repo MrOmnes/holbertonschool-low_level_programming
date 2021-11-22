ca9f403b9ffd7131f809d103dc9716d1c4574ae1
MrOmnes <3644@holbertonschool.com>
2021-11-22 13:35:03 +0000

    Adding 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*read_textfile - Read the content of a file
+*
+*@filename: name of the file
+*@letters: size of the file
+*Return: number of char printed
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fo;
+	int testread;
+	char *buffer = malloc(letters * (sizeof(char)));
+
+	if (buffer == NULL)
+		return (0);
+
+	fo = open(filename, O_RDONLY);
+
+	if (fo == -1)
+		return (0);
+
+
+	testread = read(fo, buffer, letters);
+
+	if (testread == -1)
+		return (0);
+
+	write(1, buffer, testread);
+
+	return (letters);
+}
