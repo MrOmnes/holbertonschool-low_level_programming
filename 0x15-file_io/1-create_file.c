ca3bc9588c6d3a76af8a39ff30419e089fb48828
MrOmnes <3644@holbertonschool.com>
2021-11-22 14:35:45 +0000

    Add 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,19 +1,35 @@
 #include "main.h"
 
+/**
+* create_file - Créer un fichier et alloue du contenu
+*
+*@filename: Name of the file
+*@text_content: Contenu du fichier
+*Return: 1 if success -1 if not
+*/
+
 int create_file(const char *filename, char *text_content)
 {
 	int fo;
+	int testwrite;
+	unsigned long int sizetext = strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-	{
-		fo = open(filename, "w");
+	fo = open(filename, O_WRONLY | O_CREAT);
 
-		if (fo == -1)
+	if (fo == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		testwrite = write(fo, text_content, sizetext);
+
+		if (testwrite == -1)
 			return (-1);
 	}
-	
-	return(1);
+
+	close(fo);
+	return (1);
 }
