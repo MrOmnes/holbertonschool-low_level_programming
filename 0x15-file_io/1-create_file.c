e22e01c3d1539a762fddd26730174199f10af58b
MrOmnes <3644@holbertonschool.com>
2021-11-22 15:07:53 +0000

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -39,7 +39,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	if (access(filename, F_OK ) == 0)
+	if (access(filename, F_OK) == 0)
 	{
 		fo = open(filename, O_WRONLY);
 	}
