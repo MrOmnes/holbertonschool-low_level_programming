9b6423f066677b7625b5abe5f0fde90b77c96c36
MrOmnes <3644@holbertonschool.com>
2021-11-22 15:01:53 +0000

    Try with our own _strlen

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,27 @@
 #include "main.h"
 
+
+/**
+ * _strlen - Compte le nombre de caractere d'une chaine
+ *
+ * @s: variable
+ *
+ * Return: (i)
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	return (i);
+}
+
+
+
 /**
 * create_file - Créer un fichier et alloue du contenu
 *
@@ -12,7 +34,7 @@ int create_file(const char *filename, char *text_content)
 {
 	int fo;
 	int testwrite;
-	unsigned long int sizetext = strlen(text_content);
+	unsigned long int sizetext = _strlen(text_content);
 
 	if (filename == NULL)
 		return (-1);
