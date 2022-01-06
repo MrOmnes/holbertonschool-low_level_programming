dc7474ea10e58f2c641e26e3c8955d6341f19f0c
MrOmnes <3644@holbertonschool.com>
2022-01-06 13:37:58 +0000

    fix 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,30 @@
 #include "lists.h"
 
 /**
+* dlistint_len - Check the size of a list
+*
+*@h: List to check
+*Return:  len of the list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	int number_of_node = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	while (h != NULL)
+	{
+		number_of_node++;
+		h = h->next;
+	}
+
+	return (number_of_node);
+}
+
+/**
 *get_dnodeint_at_index - Get the node at the index
 *
 *@head: Head of the node
